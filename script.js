6f803eca7757a4d145c8c7492f96bdf656b5f9bb
Brandon Palma <luisbrandonpalmamedina46@gmail.com>
2025-09-23 11:25:23 -0600

    [UPDATE]Se agrego validación al botón checkMessageButton mostrando alerta si el campo de mensaje está vacío

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result-message");
 const checkMessageButton = document.getElementById("check-message-btn");
+
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value.trim() === "") {
+    alert("Please enter a message.");
+    return; 
+  }
+
+});
